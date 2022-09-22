71d99e820460cbdd7a310ff0abc58dc733e6c47f
Satya Sourav Patel <satyasouravssp0@gmail.com>
2022-09-22 23:26:59 +0530

    Adding Leetcodes

---
+++ LeetCode/Q-1295. Find Numbers with Even Number of Digits/src/com/satya/Main.java
@@ -0,0 +1,27 @@
+package com.satya;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ans = {12,345,2,6,7896};
+        System.out.println(findNumbers(ans));
+    }
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int element : nums){
+            if(findit(element)) count++;
+        }
+        return count;
+    }
+
+    public static boolean findit(int element){
+        int n = findDigit(element);
+
+        return (n & 1) != 1;
+    }
+
+    public static int findDigit( int element){
+        if(element == 0) return 1;
+        if(element < 0) element = element * -1;
+        return (int)(Math.log10(element)) + 1;
+    }
+}
